00fbdf83eac8fb84dea42e4608dde05db7527e14
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-05-13 03:54:10 -0600

    Added a repl that starts with sequelize models in context similar to rails console.
    This is really convenient for quick manual testing.

---
+++ services/backend/repl.js
@@ -0,0 +1,52 @@
+/* A repl server for BlockedTODO.
+ * It behaves in a similar manner to rails console.
+ * Run with `node --experimental-repl-await ./repl.js` */
+const repl = require('repl');
+
+// Helper methods to add in the context of the repl
+const getOwnMethods = (object) => {
+    return Object.getOwnPropertyNames(object).filter((property) => typeof object[property] == 'function');
+};
+
+const getAllMethods = (object) => {
+    const methods = [];
+    for (const method in object) {
+        if (typeof object[method] == 'function') {
+            methods.push(method);
+        }
+    }
+    return methods;
+};
+
+const getOwnAttributes = (object) => {
+    return Object.getOwnPropertyNames(object).filter((property) => typeof object[property] != 'function');
+};
+
+const getAllAttributes = (object) => {
+    const methods = [];
+    for (const method in object) {
+        if (typeof object[method] != 'function') {
+            methods.push(method);
+        }
+    }
+    return methods;
+};
+
+const printDepth = (object, depth) => {
+    console.dir(object, {depth: depth});
+}
+
+// Create repl server
+const replServer = repl.start({
+    prompt: '> ',
+});
+
+// Import in the context of the repl server
+Object.assign(replServer.context, {
+    ...require('db/models'),
+    printDepth,
+    getOwnMethods,
+    getAllMethods,
+    getOwnAttributes,
+    getAllAttributes,
+});
