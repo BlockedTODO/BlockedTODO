8788f8ea3b35da0067e3f56f1b8d6740485ee482
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-05-13 23:19:11 -0600

    Add BlockedTODO comment for sequelize transactional model tests.

---
+++ services/backend/src/db/models/index.test.js
@@ -0,0 +1,8 @@
+/* Test models (validations, associations, scopes, cascade on delete, etc.)
+ * BlockedTODO: https://github.com/sequelize/sequelize/issues/11408
+ * Add model tests using transactional tests when the issue is resolved
+ * The only solution right now is truncating or hacking the 'it' function which are both sub-optimal */
+
+test('sample test', () => {
+    expect(true).toBe(true);
+});
