5654905120dd716632232ae3b1194f4bacee4bab
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-05-17 01:57:17 -0600

    Add ability to attach a vscode debugger to running applications (in dev mode).

--- services/backend/scripts/docker-start-dev.sh
+++ services/backend/scripts/docker-start-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Install missing dependencies (so we don't have to rebuild at every package*.json change)
-npm install --no-progress
+npm install --no-progress --no-audit
 
 # Wait for services
 dockerize -wait tcp://$DATABASE_HOST:$DATABASE_PORT -timeout 1m
@@ -10,4 +10,4 @@ dockerize -wait tcp://$DATABASE_HOST:$DATABASE_PORT -timeout 1m
 npx sequelize db:migrate
 
 # Start server
-npm run start
+npm run start:debug
