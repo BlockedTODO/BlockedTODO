1d70e7dea617b9964ede02a0dee3c72acd751a6b
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-05-18 02:28:18 -0600

    Add some default styles and assets.

--- services/frontend/src/App.js
+++ services/frontend/src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BlockedTodoLogoIcon} from 'assets/icons';
 import './App.scss';
 
 const App = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+        <div className='App'>
+            <header className='App-header'>
+                <BlockedTodoLogoIcon className='App-logo' alt='logo' />
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
             </header>
         </div>
     );
