42d60780a73380f508a30771b66508ee59d0049d
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-05-19 12:51:13 -0600

    Pass environment variables to frontend.

--- services/frontend/scripts/docker-start.sh
+++ services/frontend/scripts/docker-start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Wait for services
-dockerize -wait tcp://$BACKEND_HOST:$BACKEND_PORT -timeout 1m
+dockerize -wait tcp://$REACT_APP_BACKEND_HOST:$REACT_APP_BACKEND_PORT -timeout 1m
 
 # Start server
 npm run start || echo "Frontend server stopped"
