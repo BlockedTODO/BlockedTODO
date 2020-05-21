a5d55c0b793496fbed3c37d6706295d4200de3cf
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-05-21 05:11:20 -0600

    Add task objection model and migration.

---
+++ services/backend/src/db2/migrations/20200521050527_create-task.js
@@ -0,0 +1,14 @@
+exports.up = async (knex) => {
+    await knex.schema.createTable('tasks', (table) => {
+        table.uuid('id').primary().notNullable();
+        table.string('url').notNullable().unique();
+        table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
+        table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable();
+
+        table.index('url');
+    });
+};
+
+exports.down = async (knex) => {
+    await knex.schema.dropTableIfExists('tasks');
+};
