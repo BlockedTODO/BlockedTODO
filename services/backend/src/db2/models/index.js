4e78f0f1757bff0a203cd26347e01fb2d203dd54
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-05-21 05:11:20 -0600

    Add task objection model and migration.

--- services/backend/src/db2/models/index.js
+++ services/backend/src/db2/models/index.js
@@ -1,9 +1,11 @@
 const User = require('./user');
 const Repository = require('./repository');
 const Issue = require('./issue');
+const Task = require('./task');
 
 module.exports = {
     User,
     Repository,
     Issue,
+    Task,
 };
---
+++ services/backend/src/db2/models/task.js
@@ -0,0 +1,29 @@
+const {compose} = require('objection');
+const guid = require('objection-guid');
+const BaseModel = require('./baseModel');
+
+const mixins = compose(
+    guid(),
+);
+
+class Task extends mixins(BaseModel) {
+    static get tableName() {
+        return 'tasks';
+    }
+
+    // Whenever a model instance is created it is checked against this schema for validation.
+    static get jsonSchema() {
+        return {
+            type: 'object',
+            required: ['url'],
+
+            properties: {
+                url: {type: 'string', format: 'uri'},
+                createdAt: {type: 'string', format: 'date-time'},
+                updatedAt: {type: 'string', format: 'date-time'}
+            }
+        }
+    }
+}
+
+module.exports = Task;
