5b7080cb5a6e5bb351c188f3715a36ce4f30dffe
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-05-21 06:31:27 -0600

    Add task objection associations and migration.

---
+++ services/backend/src/db2/migrations/20200521054925_add-task-associations.js
@@ -0,0 +1,16 @@
+exports.up = async (knex) => {
+    await knex.schema.table('tasks', (table) => {
+        table.uuid('repository_id').references('id').inTable('repositories').onDelete('CASCADE').onUpdate('CASCADE').notNullable();
+        table.uuid('issue_id').references('id').inTable('issues').onDelete('CASCADE').onUpdate('CASCADE').notNullable();
+
+        table.index('repository_id');
+        table.index('issue_id');
+    });
+};
+
+exports.down = async (knex) => {
+    await knex.schema.table('tasks', (table) => {
+        table.dropColumn('repository_id');
+        table.dropColumn('issue_id');
+    });
+};
