2a783e636bf255aeb5588547e5e4a1f59c64f7af
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-05-26 10:41:15 -0600

    Update login page UI.

--- services/frontend/src/components/Logo/Logo.js
+++ services/frontend/src/components/Logo/Logo.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import classNames from 'classnames';
 import {Link} from 'react-router-dom';
 import {ToDoIcon} from 'assets/icons';
 import './Logo.scss';
 
-const Logo = ({route = '/'}) => (
-    <Link className='logo-container' to={route}>
+const Logo = ({className, route = '/'}) => (
+    <Link className={classNames('logo-container', className)} to={route}>
         <ToDoIcon className='logo-icon' />
         <h3> {/* eslint-disable-next-line react/jsx-no-comment-textnodes */}
             <code className='logo-text'>// BlockedTODO</code>
