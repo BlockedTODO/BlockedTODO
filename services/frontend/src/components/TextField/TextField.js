87b0f31f46fb90581df2352089f7b2d03e77796b
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-05-27 09:34:33 -0600

    Update textfield to have no hover effects when disabled.
    Implementation details: Use material-ui textfield directly instead of its subcomponents.

--- services/frontend/src/components/TextField/TextField.js
+++ services/frontend/src/components/TextField/TextField.js
@@ -1,32 +1,29 @@
 import React from 'react';
-import {FormControl, OutlinedInput, InputLabel, FormHelperText} from '@material-ui/core';
+import {TextField as MaterialTextField} from '@material-ui/core';
 import './TextField.scss';
 
 /* Custom text field as per https://material-ui.com/components/text-fields/#customized-inputs
  * Using scss styling as per https://material-ui.com/guides/interoperability/#plain-css */
-const TextField = ({label='', helperText='', ...props}) => (
+const TextField = (props) => (
     <div className='text-field'>
-        <FormControl
+        <MaterialTextField
             variant='outlined'
             size='small'
+            InputProps={{
+                classes: {
+                    root: props.disabled ? 'input-disabled' : 'input',
+                    notchedOutline: 'input-notched-outline',
+                    focused: 'input-focused',
+               }
+            }}
+            InputLabelProps={{
+                classes: {
+                    root: 'label',
+                    focused: 'label-focused',
+                }
+            }}
             {...props}
-        >
-            <InputLabel htmlFor={label} classes={{root: 'label', focused: 'focused'}}>{label}</InputLabel>
-            <OutlinedInput
-                id={label}
-                onChange={() => {}}
-                label={label}
-                aria-describedby='helper-text'
-                error
-                classes={{
-                    root: 'root',
-                    focused: 'focused',
-                    notchedOutline: 'notched-outline',
-                    input: 'input'
-                }}
-            />
-            <FormHelperText id='helper-text'>{helperText}</FormHelperText>
-        </FormControl>
+        />
     </div>
 );
 
