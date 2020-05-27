1fd298951513d2e87f267f776283ea667b6d2c63
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-05-27 07:28:52 -0600

    Add first version of textfield.

--- services/frontend/src/components/index.js
+++ services/frontend/src/components/index.js
@@ -3,3 +3,4 @@ export {default as Logo} from './Logo';
 export {default as UserAvatar} from './UserAvatar';
 export {default as Spinner} from './Spinner';
 export {default as LoadingButton} from './LoadingButton';
+export {default as TextField} from './TextField';
