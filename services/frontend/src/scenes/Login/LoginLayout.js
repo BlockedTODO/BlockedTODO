5e46feeb05857d5ac20b774ce3a0dd53a738b12a
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-05-27 10:28:39 -0600

    Add on submit handler for login form.

--- services/frontend/src/scenes/Login/LoginLayout.js
+++ services/frontend/src/scenes/Login/LoginLayout.js
@@ -2,24 +2,23 @@ import React from 'react';
 import {Logo, LoadingButton, TextField} from 'components/';
 import './Login.scss';
 
-const LoginLayout = () => (
+const LoginLayout = (props) => (
     <div className='login'>
         <Logo className='login-logo' />
-        <LoginCard />
+        <LoginCard {...props} />
     </div>
 );
 
-const LoginCard = () => (
+const LoginCard = ({emailInput, passwordInput, onSubmit}) => (
     <div className='login-card'>
         <CardTitle />
 
         <div className='form-container card-section'>
-            <TextField className='form-input' label='Email' type='email' />
-
-            <TextField className='form-input' label='Password' type='password' />
+            <TextField className='form-input' label='Email' type='email' {...emailInput} />
+            <TextField className='form-input' label='Password' type='password' {...passwordInput} />
 
             <div className='buttons-container'>
-                <LoadingButton className='login-button' label='login' />
+                <LoadingButton className='login-button' label='login' onClick={onSubmit}/>
                 <button className='signup-button'>create account</button>
             </div>
         </div>
