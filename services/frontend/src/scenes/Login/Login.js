085162eafd77b57d0617f1b1e32d2ecd2a436f58
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-05-29 22:53:36 -0600

    Redirect on successful login.

--- services/frontend/src/scenes/Login/Login.js
+++ services/frontend/src/scenes/Login/Login.js
@@ -1,8 +1,9 @@
 import React, {useState} from 'react';
-import LoginLayout from './LoginLayout';
+import {useHistory} from 'react-router-dom';
 import {useInput} from 'utils/hooks';
 import {useLazyQuery} from '@apollo/react-hooks';
 import {gql} from 'apollo-boost';
+import LoginLayout from './LoginLayout';
 
 const loginQuery = gql`
     query User($email: String!, $password: String!){
@@ -18,9 +19,16 @@ const Login = () => {
     const emailInput = useInput();
     const passwordInput = useInput();
     const [error, setError] = useState('');
+    const history = useHistory();
 
+    /* FIXME: the current login implementation still has some vulnerabilities that need to be fixed before a full release.
+    * Follow the instructions here: https://hasura.io/blog/best-practices-of-using-jwt-with-graphql/#intro */
+    const onSuccessfulLogin = ({login}) => {
+        localStorage.setItem('authentication_token', login.token);
+        history.push('/');
+    }
     const [getUser, {loading}] = useLazyQuery(loginQuery, {
-        onCompleted: ({login}) => localStorage.setItem('authentication_token', login.token),
+        onCompleted: onSuccessfulLogin,
         onError: (e) => setError(e.message)
     });
 
