7a6cdeec90f2edf568a66770df5ba643bdc9e4e8
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-05-30 17:08:54 -0600

    Add signup page.

---
+++ services/frontend/src/scenes/Signup/SignupLayout.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Logo, LoadingButton, TextField, FormError} from 'components/';
+import './Signup.scss';
+
+const SignupLayout = (props) => (
+    <div className='signup'>
+        <Logo className='signup-logo' />
+        <SignupCard {...props} />
+    </div>
+);
+
+const SignupCard = ({emailInput, passwordInput, onSubmit, isLoading, errorMessage}) => (
+    <div className='signup-card'>
+        <CardTitle />
+        <FormError message={errorMessage} />
+
+        <div className='form-container card-section'>
+            <TextField className='form-input' label='Email' type='email' {...emailInput} />
+            <TextField className='form-input' label='Password' type='password' {...passwordInput} />
+
+            <div className='buttons-container'>
+                <LoadingButton className='signup-button' label='create account' onClick={onSubmit} isLoading={isLoading} />
+                <button className='login-button'>login</button>
+            </div>
+        </div>
+    </div>
+);
+
+const CardTitle = () => (
+    <div className='card-title'>
+        <h1 className='card-section'>Signup</h1>
+    </div>
+);
+
+export default SignupLayout;
--- services/frontend/src/scenes/index.js
+++ services/frontend/src/scenes/index.js
@@ -1,3 +1,4 @@
+export {default as Signup} from './Signup';
 export {default as Login} from './Login';
 export {default as NotFound} from './NotFound';
 export {default as Repositories} from './Repositories';
