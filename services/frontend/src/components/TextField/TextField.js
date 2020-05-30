382d701650fa4de5aa8ecb5c86c56307dc0ad425
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-05-29 23:45:43 -0600

    Make textfield component have consistent styles when error is set to true.

--- services/frontend/src/components/TextField/TextField.js
+++ services/frontend/src/components/TextField/TextField.js
@@ -4,27 +4,36 @@ import './TextField.scss';
 
 /* Custom text field as per https://material-ui.com/components/text-fields/#customized-inputs
  * Using scss styling as per https://material-ui.com/guides/interoperability/#plain-css */
-const TextField = (props) => (
-    <div className='text-field'>
-        <MaterialTextField
-            variant='outlined'
-            size='small'
-            InputProps={{
-                classes: {
-                    root: props.disabled ? 'input-disabled' : 'input',
-                    notchedOutline: 'input-notched-outline',
-                    focused: 'input-focused',
-                }
-            }}
-            InputLabelProps={{
-                classes: {
-                    root: 'label',
-                    focused: 'label-focused',
-                }
-            }}
-            {...props}
-        />
-    </div>
-);
+const TextField = (props) => {
+    let containerClass = 'text-field'
+    if (props.disabled) {
+        containerClass = 'text-field-disabled'
+    } else if (props.error) {
+        containerClass = 'text-field-error'
+    }
+
+    return (
+        <div className={containerClass}>
+            <MaterialTextField
+                variant='outlined'
+                size='small'
+                InputProps={{
+                    classes: {
+                        root: 'input',
+                        notchedOutline: 'input-notched-outline',
+                        focused: 'input-focused',
+                    }
+                }}
+                InputLabelProps={{
+                    classes: {
+                        root: 'label',
+                        focused: 'label-focused',
+                    }
+                }}
+                {...props}
+            />
+        </div>
+    );
+};
 
 export default TextField;
