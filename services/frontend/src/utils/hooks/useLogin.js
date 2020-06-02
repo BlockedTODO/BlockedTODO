1aa683d19fdd8c2f9a0960807373686d9aa336d3
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-06-01 19:54:40 -0600

    Make login secure (at the expense of user experience).
    The authentication token is now stored in memory instead of local storage. This means login is now perfectly secure, but page refreshes cause the user to lose their session.
    
    In the future, we will want to persist sessions with a refresh token that can be securely stored in localstorage.
    
    We will also probably use something other than the apollo client in-memory cache (eg. react context api) because it doesn't look like it's intended to be read directly as I am doing.
    
    Despite those drawbacks, I prefer putting security above usability until I have time to implement the best of both worlds solution, as outlined in the comment I left in useLogin.js.

--- services/frontend/src/utils/hooks/useLogin.js
+++ services/frontend/src/utils/hooks/useLogin.js
@@ -1,15 +1,17 @@
 import {useHistory} from 'react-router-dom';
-import {useLazyQuery} from '@apollo/react-hooks';
+import {useApolloClient, useLazyQuery} from '@apollo/react-hooks';
 import {loginQuery} from 'graphql/operations/';
 
 const useLogin = ({onSuccess = () => {}, onError = () => {}}) => {
+    const client = useApolloClient();
     const history = useHistory();
 
-    /* FIXME: the current login implementation still has some vulnerabilities that need to be fixed before a full release.
-    * Follow the instructions here: https://hasura.io/blog/best-practices-of-using-jwt-with-graphql/#intro */
+    /* The current login implementation is secure but offers a poor user experience.
+     * Sessions are not persisted on refresh because we store the authentication token in memory via the apollo in-memory store.
+     * TODO: Follow the instructions here: https://hasura.io/blog/best-practices-of-using-jwt-with-graphql/#intro */
     const onLogin = ({login}) => {
         onSuccess();
-        localStorage.setItem('authentication_token', login.token);
+        client.writeData({data: {authentication_token: login.token}});
         history.push('/');
     };
 
