a6f3e8775515046fe6366d49986d6a40c6554834
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-06-04 18:03:54 -0600

    Refactor: split out github webhook handlers into multiple files.

---
+++ services/backend/src/github/webhooks/issue.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+const app = require('github/app');
+
+const onIssueCommentCreated = async ({payload}) => {
+    const installationId = payload.installation.id;
+    const repositoryId = payload.repository.node_id;
+    const installationAccessToken = await app.getInstallationAccessToken({
+        installationId,
+    });
+    data = {
+        "query":`
+            mutation {
+                createIssue(input: {repositoryId: "${repositoryId}", title: "Auto-generated issue (using code!)"}) {
+                    issue {
+                        id
+                        bodyText
+                    }
+                }
+            }
+        `,
+        "variables": {}
+    };
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${installationAccessToken}`,
+    }
+
+    try {
+        const response = await axios.post('https://api.github.com/graphql', data, {headers: headers});
+        console.dir(response);
+    } catch (e) {
+        console.dir(e.response);
+    }
+};
+
+module.exports = {onIssueCommentCreated};
