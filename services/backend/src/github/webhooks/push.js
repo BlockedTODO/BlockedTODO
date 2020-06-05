78a79302fef5857950df6ae888803571c7f72041
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-06-05 03:00:46 -0400

    Create temp file, download repository as zip, and extract zip archive on every push to the default branch.

--- services/backend/src/github/webhooks/push.js
+++ services/backend/src/github/webhooks/push.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
+const stream = require('stream');
+const {promisify} = require('util');
 const unzipper = require('unzipper');
+const globby = require('globby');
+const tempy = require('tempy');
+const del = require('del');
 const createAppClient = require('github/axiosWrapper');
 const graphqlRequestBody = require('utils/graphqlRequestBody');
+
+const pipeline = promisify(stream.pipeline);
 
 const onPush = async ({payload}) => {
     const defaultBranch = payload.repository.default_branch;
@@ -36,10 +43,34 @@ const onPush = async ({payload}) => {
     const urlResponse = await github.post('/graphql', getArchiveUrl);
     const downloadLink = urlResponse.data.data.node.defaultBranchRef.target.zipballUrl;
 
-    // Download and extract file
-    const fileResponse = await github.get(downloadLink, {responseType: 'stream'});
-    fileResponse.data.pipe(fs.createWriteStream('/downloadedrepo.zip'));
-    fs.createReadStream('/downloadedrepo.zip').pipe(unzipper.Extract({path: '/extractedrepo'}));
+    const codeFolder = await downloadRepository(github, downloadLink);
+    console.log(codeFolder);
+
+    // Delete temp folder
+};
+
+
+const downloadRepository = async (githubClient, downloadLink) => {
+    const tempFolder = tempy.directory();
+    const zipLocation = `${tempFolder}/repository.zip`;
+
+    // Download zip file
+    const fileResponse = await githubClient.get(downloadLink, {responseType: 'stream'});
+    fileResponse.data.pipe(fs.createWriteStream(zipLocation));
+
+    // Extract files
+    await pipeline(
+        fs.createReadStream(zipLocation),
+        unzipper.Extract({path: tempFolder}),
+    );
+
+    // Delete zip file
+    await del(zipLocation, {force: true});
+
+    // The archive is unzipped as a single folder with a random name that contains the code
+    const codeFolder = (await globby(`${tempFolder}/*`, {onlyDirectories: true}))[0];
+
+    return codeFolder;
 };
 
 module.exports = {onPush};
