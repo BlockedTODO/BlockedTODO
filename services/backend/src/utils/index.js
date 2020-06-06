d2ae9c234bcf6af56662fab04ba7be1705b73243
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-06-05 23:17:24 -0400

    Revert "Remove asyncUnzip helper function, will use promisified stream pipelines instead."
    This reverts commit 3a614e8f922454107437298a2fd198aca33f54ea.

--- services/backend/src/utils/index.js
+++ services/backend/src/utils/index.js
@@ -1,7 +1,9 @@
 const logger = require('./logger');
 const urlNormalizer = require('./urlNormalizer');
+const asyncUnzip = require('./asyncUnzip');
 
 module.exports = {
     logger,
     urlNormalizer,
+    asyncUnzip,
 };
