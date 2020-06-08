e640ed2b3ae7b8fd5118b8da337cfe3c15c7e754
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-06-08 01:51:10 -0400

    Scan all files for comments, compile list of comments in an array.

--- services/backend/src/utils/index.js
+++ services/backend/src/utils/index.js
@@ -1,9 +1,11 @@
 const logger = require('./logger');
 const urlNormalizer = require('./urlNormalizer');
 const asyncUnzip = require('./asyncUnzip');
+const COMMENT_REGEX = require('./commentRegex');
 
 module.exports = {
     logger,
     urlNormalizer,
     asyncUnzip,
+    COMMENT_REGEX,
 };
