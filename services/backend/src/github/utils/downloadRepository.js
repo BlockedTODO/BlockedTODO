6e94016d79dd050d90f9a79af79d31d46727a605
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-06-09 03:31:02 -0400

    Refactor code such that less business logic is present in push.js (move code to a bunch of reusable utils functions).
    Also make push.js call parser.

---
+++ services/backend/src/github/utils/downloadRepository.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const globby = require('globby');
+const tempy = require('tempy');
+const {asyncUnzip, logger} = require('utils/');
+const graphqlRequestBody = require('utils/graphqlRequestBody');
+
+const fsPromises = fs.promises;
+
+// Get a temporary repository download URL
+const repositoryDownloadLink = async (githubClient, repositoryId) => {
+    const getArchiveUrl = graphqlRequestBody(`
+        query {
+            node(id: "${repositoryId}") {
+                ... on Repository {
+                    defaultBranchRef {
+                        target {
+                            ... on Commit {
+                                zipballUrl
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    `);
+
+    const urlResponse = await githubClient.post('/graphql', getArchiveUrl);
+    const downloadLink = urlResponse.data.data.node.defaultBranchRef.target.zipballUrl;
+
+    return downloadLink;
+};
+
+// Download repository, return the folder containing the downloaded & extracted repository
+const downloadRepository = async (githubClient, repositoryId) => {
+    const tempFolder = tempy.directory();
+    const zipLocation = `${tempFolder}/repository.zip`;
+
+    // Download zip file
+    const downloadLink = await repositoryDownloadLink(githubClient, repositoryId);
+    const fileResponse = await githubClient.get(downloadLink, {responseType: 'stream'});
+    fileResponse.data.pipe(fs.createWriteStream(zipLocation));
+
+    // Extract files
+    try {
+        await asyncUnzip(zipLocation, tempFolder);
+    } catch (error) {
+        logger.error(`Error occurred during asyncUnzip: ${error}`);
+    }
+
+    // Delete zip file
+    await fsPromises.unlink(zipLocation);
+
+    // The archive is unzipped as a single folder with a random name that contains the code
+    const codeFolder = (await globby(`${tempFolder}/*`, {onlyDirectories: true}))[0];
+
+    return codeFolder;
+};
+
+module.exports = downloadRepository;
--- services/backend/src/github/webhooks/push.js
+++ services/backend/src/github/webhooks/push.js
@@ -1,11 +1,5 @@
-const fs = require('fs');
-const {asyncUnzip, logger, COMMENT_REGEX} = require('utils/');
-const globby = require('globby');
-const tempy = require('tempy');
-const createAppClient = require('github/axiosWrapper');
-const graphqlRequestBody = require('utils/graphqlRequestBody');
-
-const fsPromises = fs.promises;
+const parseCodebase = require('parser');
+const {createAppClient, downloadRepository} = require('github/utils/');
 
 const onPush = async ({payload}) => {
     const defaultBranch = payload.repository.default_branch;
@@ -16,75 +10,15 @@ const onPush = async ({payload}) => {
         return;
     }
 
-    const github = await createAppClient(payload.installation.id);
+    const githubClient = await createAppClient(payload.installation.id);
     const repositoryId = payload.repository.node_id;
 
-    // Get a temporary repo download url
-    const getArchiveUrl = graphqlRequestBody(`
-        query {
-            node(id: "${repositoryId}") {
-                ... on Repository {
-                    defaultBranchRef {
-                        target {
-                            ... on Commit {
-                                zipballUrl
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    `);
+    const codeFolder = await downloadRepository(githubClient, repositoryId);
 
-    const urlResponse = await github.post('/graphql', getArchiveUrl);
-    const downloadLink = urlResponse.data.data.node.defaultBranchRef.target.zipballUrl;
-
-    const codeFolder = await downloadRepository(github, downloadLink);
-
-    const filesToScan = await globby(`${codeFolder}/**/*`);
-    logger.info(`files to scan: ${filesToScan}`);
-
-    const comments = [];
-
-    // Create promises for each file to scan
-    const scanComments = filesToScan.map(async (file) => {
-        try {
-            logger.info(`Gathering comments from file: ${file}`);
-            const contents = await fsPromises.readFile(file, {encoding: 'utf-8'});
-            const fileComments = contents.match(COMMENT_REGEX) || [];
-            comments.push(...fileComments);
-        } catch (error) {
-            logger.error(`Error occurred while reading file ${file}: ${error.toString()}`);
-            throw error;
-        }
-    });
-
-    // Scan all files asynchronously, continue when all files are scanned.
-    await Promise.allSettled(scanComments);
-
-    logger.info(comments);
+    const referencedIssues = await parseCodebase(codeFolder);
+    console.dir(referencedIssues);
 
     // Delete temp folder
-};
-
-const downloadRepository = async (githubClient, downloadLink) => {
-    const tempFolder = tempy.directory();
-    const zipLocation = `${tempFolder}/repository.zip`;
-
-    // Download zip file
-    const fileResponse = await githubClient.get(downloadLink, {responseType: 'stream'});
-    fileResponse.data.pipe(fs.createWriteStream(zipLocation));
-
-    // Extract files
-    await asyncUnzip(zipLocation, tempFolder);
-
-    // Delete zip file
-    await fsPromises.unlink(zipLocation);
-
-    // The archive is unzipped as a single folder with a random name that contains the code
-    const codeFolder = (await globby(`${tempFolder}/*`, {onlyDirectories: true}))[0];
-
-    return codeFolder;
 };
 
 module.exports = {onPush};
