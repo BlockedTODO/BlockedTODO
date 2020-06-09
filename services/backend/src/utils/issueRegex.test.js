15b4b52d67a6cf790ced4d0b94f99330eb734dce
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-06-09 02:26:50 -0400

    Add more unit tests for issue regex.

--- services/backend/src/utils/issueRegex.test.js
+++ services/backend/src/utils/issueRegex.test.js
@@ -16,6 +16,22 @@ describe('Prefix regex', () => {
         expect(results.length).toBe(1);
         expect(results[0].groups.prefix).toEqual('blockedtodo: ');
     });
+
+    it('does not match if prefix is missing', () => {
+        const code = '// https://github.com/BlockedTODO/BlockedTODO/issues/57';
+        const results = Array.from(code.matchAll(ISSUE_REGEX));
+
+        expect(results.length).toBe(0);
+    });
+
+    it('matches several prefixes in one comment', () => {
+        const code = `/* BlockedTODO: github.com\nBlockedTODO: github.com */`;
+        const results = Array.from(code.matchAll(ISSUE_REGEX));
+
+        expect(results.length).toBe(2);
+        expect(results[0].groups.prefix).toEqual('BlockedTODO: ');
+        expect(results[1].groups.prefix).toEqual('BlockedTODO: ');
+    });
 });
 
 describe('URL regex', () => {
@@ -35,5 +51,22 @@ describe('URL regex', () => {
 
         expect(results.length).toBe(1);
         expect(results[0].groups.url).toEqual(issueUrl);
+    });
+
+    it('does not match if no url is present', () => {
+        const code = '// BlockedTODO: hello world';
+        const results = Array.from(code.matchAll(ISSUE_REGEX));
+
+        expect(results.length).toBe(0);
+    });
+
+    it('matches several issue urls in one comment', () => {
+        const issueUrl = 'https://github.com/BlockedTODO/BlockedTODO/issues/57'
+        const code = `/* BlockedTODO: ${issueUrl}\nBlockedTODO: ${issueUrl} */`;
+        const results = Array.from(code.matchAll(ISSUE_REGEX));
+
+        expect(results.length).toBe(2);
+        expect(results[0].groups.url).toEqual(issueUrl);
+        expect(results[1].groups.url).toEqual(issueUrl);
     });
 });
