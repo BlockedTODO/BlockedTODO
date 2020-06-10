aa0d28cc581245cb0b5da7fcd48d31c737ded0e3
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-06-10 02:18:40 -0400

    Delete unreferenced issues on push.

--- services/backend/src/github/webhooks/push.js
+++ services/backend/src/github/webhooks/push.js
@@ -3,6 +3,7 @@ const {promises: fsPromises} = require('fs');
 const parseCodebase = require('parser');
 const logger = require('utils/logger');
 const {createAppClient, downloadRepository} = require('github/utils/');
+const {Issue, Repository} = require('db/models');
 
 const onPush = async ({payload}) => {
     const defaultBranch = payload.repository.default_branch;
@@ -22,8 +23,40 @@ const onPush = async ({payload}) => {
     const referencedIssues = await parseCodebase(codeFolder);
     logger.info(referencedIssues);
 
+    // Get repository and log its related issues
+    let repository = await Repository.query().findOne({hostId: repositoryId}).withGraphFetched('issues');
+    logger.info(repository);
+
+    await deleteUnreferencedIssues(repository, referencedIssues);
+
+    // Refresh repository so that deleted issues are removed
+    repository = await repository.$query().withGraphFetched('issues');
+    logger.info(repository);
+
     // Delete temp folder
     await fsPromises.rmdir(destination, {recursive: true});
+};
+
+/* Unrelate issues that are no longer mentioned in the codebase from the repository.
+ * Delete the issue if the issue is no longer referenced by any repositories. */
+const deleteUnreferencedIssues = async (repository, referencedIssues) => {
+    const issues = await repository.$relatedQuery('issues');
+    for (let issue of issues) {
+        if (issue.url in referencedIssues) {
+            continue;
+        }
+
+        await Issue.transaction(async (_tx) => {
+            /* By making this code atomic via a transaction, we avoid race conditions that may
+             * occur between the check for issue.repositories.length and the issue deletion. */
+            await issue.$relatedQuery('repositories').unrelate().where({repositoryId: repository.id});
+
+            issue = await issue.$query().withGraphFetched('repositories');
+            if (issue.repositories.length === 0) {
+                await issue.$query().delete();
+            }
+        });
+    }
 };
 
 module.exports = {onPush};
