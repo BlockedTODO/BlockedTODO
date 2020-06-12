5c0e29a7bca29f9d77df464e47f2b0398a359f4c
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-06-12 03:16:45 -0400

    Scan repositories on installation.created and installation_repositories.added.

--- services/backend/src/github/webhooks/installation.js
+++ services/backend/src/github/webhooks/installation.js
@@ -1,24 +1,19 @@
 const {Repository, User} = require('db/models');
 const {findOrCreate} = require('db/utils/');
+const {createAppClient, downloadAndScan} = require('github/utils/');
 
 const onInstallationCreated = async ({payload}) => {
-    // Associate all repositories with user0 for dev testing
-    const user0 = await User.query().findOne({email: 'test0@test.com'});
+    const githubClient = await createAppClient(payload.installation.id);
 
-    for (const {node_id: hostId} of payload.repositories) {
-        await findOrCreate(Repository, {host: 'github', hostId: hostId}, async (repository) => {
-            // In the future, users will be created when they "Sign in with GitHub" on the website
-            await repository.$relatedQuery('users').relate([user0]);
-        });
-    }
-
-    // Run initial scan
+    await Promise.allSettled(payload.repositories.map(({node_id}) => {
+        createAndScanRepository(node_id, githubClient);
+    }));
 };
 
 const onInstallationDeleted = async ({payload}) => {
     /* BlockedTODO: https://github.community/t/116562
-    * installation.deleted payload does not contain a repositories array.
-    * Add installation.deleted implementation when the API is updated. */
+     * installation.deleted payload does not contain a repositories array.
+     * Add installation.deleted implementation when the API is updated. */
     if (!payload.repositories) {
         return;
     }
@@ -29,17 +24,11 @@ const onInstallationDeleted = async ({payload}) => {
 };
 
 const onInstallationRepositoriesAdded = async ({payload}) => {
-    // Associate all repositories with user0 for dev testing
-    const user0 = await User.query().findOne({email: 'test0@test.com'});
+    const githubClient = await createAppClient(payload.installation.id);
 
-    for (const {node_id: hostId} of payload.repositories_added) {
-        await findOrCreate(Repository, {host: 'github', hostId: hostId}, async (repository) => {
-            // In the future, users will be created when they "Sign in with GitHub" on the website
-            await repository.$relatedQuery('users').relate([user0]);
-        });
-    }
-
-    // Run initial scan
+    await Promise.allSettled(payload.repositories_added.map(({node_id}) => {
+        createAndScanRepository(node_id, githubClient);
+    }));
 };
 
 const onInstallationRepositoriesRemoved = async ({payload}) => {
@@ -47,6 +36,11 @@ const onInstallationRepositoriesRemoved = async ({payload}) => {
         await Repository.query().delete().where({host: 'github', hostId: hostId});
     }
 };
+
+const createAndScanRepository = async (repositoryHostId, githubClient) => {
+    await findOrCreate(Repository, {host: 'github', hostId: repositoryHostId});
+    await downloadAndScan(githubClient, repositoryHostId);
+}
 
 module.exports = {
     onInstallationCreated,
