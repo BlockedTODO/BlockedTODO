d8abb06ca8add9f9c873cf52defa78faab24b0bf
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-06-17 07:54:36 -0400

    Pass environment variables to backend tests build step.

--- terraform/modules/cicd/main.tf
+++ terraform/modules/cicd/main.tf
@@ -18,5 +18,10 @@ resource "google_cloudbuild_trigger" "filename-trigger" {
     substitutions = {
         _BACKEND_SERVICE_NAME = var.backend_service_name
         _FRONTEND_SERVICE_NAME = var.frontend_service_name
+        _DATABASE_NAME = var.database_name
+        _DATABASE_USER = var.database_user
+        _DATABASE_PASSWORD = var.database_password
+        _DATABASE_HOST = var.database_host
+        _DATABASE_PORT = var.database_port
     }
 }
