714851dd2078567c8599a71fc76c92ee56acc4d9
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-06-17 08:02:31 -0400

    Make use of docker-compose to run tests in cloud build.
    This with these changes, we do not need to use a production database to run tests.
    Instead, we use the same configuration as the one we run locally.

--- services/backend/src/db/index.js
+++ services/backend/src/db/index.js
@@ -6,7 +6,6 @@ const knexConfig = require('./config/knexfile');
 const logger = require('../utils/logger');
 
 const environment = process.env.NODE_ENV || 'development';
-
 const db = Knex(knexConfig[environment]);
 db.on('query', (data) => logger.info(`${data.sql} -- Bindings: ${data.bindings}`));
 
