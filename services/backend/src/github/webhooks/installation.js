e51670f1c2de8e68ae9611b9a395976b20f1a349
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-06-18 16:44:34 -0400

    Remove workaround for installation.deleted payload not containing a repositories array.
    Resolution here: https://github.community/t/116562

--- services/backend/src/github/webhooks/installation.js
+++ services/backend/src/github/webhooks/installation.js
@@ -13,13 +13,6 @@ const onInstallationCreated = async ({payload}) => {
 };
 
 const onInstallationDeleted = async ({payload}) => {
-    /* BlockedTODO: https://github.community/t/116562
-     * installation.deleted payload does not contain a repositories array.
-     * Add installation.deleted implementation when the API is updated. */
-    if (!payload.repositories) {
-        return;
-    }
-
     for (const {node_id: hostId} of payload.repositories) {
         await Repository.query().delete().where({host: 'github', hostId: hostId});
     }
