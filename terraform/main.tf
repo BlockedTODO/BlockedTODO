e86556bd17486b0eb254d6dbbe9dd2d751b3dee7
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-06-27 04:23:25 -0400

    Fetch production secrets in cloud build.

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,6 +39,8 @@ module "cicd" {
     remote_repo_owner = var.remote_repo_owner
     backend_service_name = var.backend_service_name
     frontend_service_name = var.frontend_service_name
+
+    production_secret_id = module.secrets.production_secret_id
 }
 
 module "storage" {
