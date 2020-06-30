e22f206134adf3d7e95237fde3fb5b79e8df052e
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-06-30 12:18:27 -0400

    Fix issue where all possible subdomains map to the backend service.
    Implementation details: configure domain with static ip addresses using an ingress instead of a service as per https://cloud.google.com/kubernetes-engine/docs/tutorials/configuring-domain-name-static-ip#step_2b_using_an_ingress
    
    (followed step 2b instead of step 2a)

--- terraform/modules/dns/main.tf
+++ terraform/modules/dns/main.tf
@@ -1,11 +1,11 @@
 # DNS configuration as per https://cloud.google.com/kubernetes-engine/docs/tutorials/configuring-domain-name-static-ip#step_2b_using_an_ingress
 
-resource "google_compute_address" "primary" {
+resource "google_compute_global_address" "primary" {
     name = var.ip_name
 }
 
 resource "google_dns_managed_zone" "prod" {
-    depends_on = [google_compute_address.primary]
+    depends_on = [google_compute_global_address.primary]
 
     name = var.dns_zone_name
     dns_name = "${var.domain}."
@@ -18,7 +18,7 @@ resource "google_dns_record_set" "base" {
     name = google_dns_managed_zone.prod.dns_name
     type = "A"
     ttl = 300
-    rrdatas = [google_compute_address.primary.address]
+    rrdatas = [google_compute_global_address.primary.address]
 }
 
 resource "google_dns_record_set" "wildcard" {
@@ -27,5 +27,5 @@ resource "google_dns_record_set" "wildcard" {
     name = "*.${google_dns_managed_zone.prod.dns_name}"
     type = "A"
     ttl = 300
-    rrdatas = [google_compute_address.primary.address]
+    rrdatas = [google_compute_global_address.primary.address]
 }
