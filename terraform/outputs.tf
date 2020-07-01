880ac7f0ec4aee1a25931770312ecff905716660
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-07-01 08:19:16 -0400

    Remove google cloud sql instance.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,14 +2,6 @@ output "registry_bucket_url" {
     value = module.registry.registry_bucket_url
 }
 
-output "db_connection_name" {
-    value = module.storage.connection_name
-}
-
-output "db_public_ip_address" {
-    value = module.storage.public_ip_address
-}
-
 output "cluster_name" {
     value = module.cluster.cluster_name
 }
