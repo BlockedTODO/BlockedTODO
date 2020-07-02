bee477fd625c136d2cbedfa744dbfc2eb2ab8c9c
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-07-02 06:51:31 -0400

    Set up some relevant terraform outputs.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,15 @@ output "cluster_name" {
 output "cluster_location" {
     value = module.cluster.cluster_location
 }
+
+output "ingress_ip" {
+    value = module.dns.ip_address
+}
+
+output "dns_name_servers" {
+    value = module.dns.dns_name_servers
+}
+
+output "production_secret_id" {
+    value = module.secrets.production_secret_id
+}
