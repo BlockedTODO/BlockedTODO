6397217ea89374736523cf76bac63cebe327e659
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-07-08 23:01:06 -0400

    Add relevant links on the landing page.

---
+++ services/website/src/components/Button/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classNames from 'classnames';
+import './Button.scss';
+
+const Button = ({className, label = '', onClick, ...otherProps}) => (
+    <button
+        className={classNames('button', className)}
+        onClick={onClick}
+        {...otherProps}
+    >
+        <span className='label'>{label}</span>
+    </button>
+);
+
+export default Button;
