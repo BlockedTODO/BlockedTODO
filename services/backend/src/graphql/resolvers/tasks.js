d55bb1c1e0e108f8c0a5e04a9e17577f669ce7e3
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-07-24 00:31:29 -0400

    Clean up some large object instantiations.

--- services/backend/src/graphql/resolvers/tasks.js
+++ services/backend/src/graphql/resolvers/tasks.js
@@ -6,7 +6,7 @@ const taskMutations = {
     createTask: async (parent, {taskInput}, {Task}, info) => {
         const {host, hostId, repositoryId, issueId} = taskInput;
 
-        return await Task.query().insert({host: host, hostId: hostId, repositoryId: repositoryId, issueId: issueId});
+        return await Task.query().insert({host, hostId, repositoryId, issueId});
     }
 };
 
