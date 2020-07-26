c710b6cf1adb74d153cb8cc205bc5ec4e20ebfb5
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-07-25 20:59:14 -0400

    Add findOrInsert method for the BaseModel.

--- services/backend/src/db/models/baseModel.js
+++ services/backend/src/db/models/baseModel.js
@@ -1,11 +1,34 @@
-const {compose, Model} = require('objection');
+const {compose, Model, QueryBuilder, UniqueViolationError} = require('objection');
 const guid = require('objection-guid');
+
+class BaseQueryBuilder extends QueryBuilder {
+    async findOrInsert(params) {
+        const instance = await this.findOne(params);
+
+        if (instance) { // instance is found, return it
+            return instance;
+        }
+
+        try { // instance is not found, try to create it
+            return await this.insert(params);
+        } catch (error) {
+            if (error instanceof UniqueViolationError) { // in case there was a race condition
+                return await this.findOne(params);
+            }
+            throw error;
+        }
+    }
+}
 
 const mixins = compose(
     guid(),
 );
 
 class BaseModel extends mixins(Model) {
+    static get QueryBuilder() {
+        return BaseQueryBuilder;
+    }
+
     $beforeInsert(...args) {
         super.$beforeInsert(...args);
         const timestamp = new Date().toISOString();
