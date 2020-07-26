014edd0c699dae0040f8d78e3e13b1a047cfd9b2
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-07-25 21:03:59 -0400

    Remove old findOrCreate helper, use findOrInsert instead.

--- services/backend/src/graphql/resolvers/repositories.js
+++ services/backend/src/graphql/resolvers/repositories.js
@@ -1,12 +1,10 @@
-const {findOrCreate} = require('db/utils/');
-
 const repositoryQueries = {
     repositories: (parent, args, {Repository}, info) => Repository.query()
 };
 
 const repositoryMutations = {
     createRepository: async (parent, {repositoryInput}, {Repository}, info) => {
-        return await findOrCreate(Repository, repositoryInput);
+        return await Repository.query().findOrInsert(repositoryInput);
     }
 };
 
