c7bcc6533e97e93ab2b3f234de3ed34ecc638006
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-07-26 23:31:14 -0400

    Add a privacy policy page.

--- services/website/src/AppRouter.js
+++ services/website/src/AppRouter.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Route, Router, Redirect, Switch} from 'react-router-dom';
 import {createBrowserHistory} from 'history';
-import {Landing, NotFound} from './scenes';
+import {Landing, Policy, NotFound} from './scenes';
 
 const AppRouter = () => (
     <Router history={createBrowserHistory({forceRefresh: false})}>
         <Switch>
             <Route path='/404' component={NotFound} />
+            <Route path='/policy' component={Policy} />
             <Route path='/' component={Landing} exact={true} />
             <Redirect from='*' to='/404' />
         </Switch>
--- services/website/src/scenes/index.js
+++ services/website/src/scenes/index.js
@@ -1,2 +1,3 @@
 export {default as NotFound} from './NotFound';
 export {default as Landing} from './Landing';
+export {default as Policy} from './Policy';
