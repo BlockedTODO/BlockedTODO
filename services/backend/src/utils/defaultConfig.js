05b6f02912c615a0d3341e90dc97a8ad47dffd67
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-07-28 00:15:20 -0400

    Add default config util.

---
+++ services/backend/src/utils/defaultConfig.js
@@ -0,0 +1,5 @@
+const DEFAULT_CONFIG = {
+    comment_prefixes: ['BlockedTODO:', 'NOTIFY:', 'Blocked by', 'Waiting on'], // Comment prefixes are case insensitive
+};
+
+module.exports = DEFAULT_CONFIG;
