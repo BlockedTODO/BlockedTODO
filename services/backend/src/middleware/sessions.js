924aa15631a2179dc2bb7eed7e9528da5da67f06
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-08-04 22:11:12 -0400

    Tweak session settings.

--- services/backend/src/middleware/sessions.js
+++ services/backend/src/middleware/sessions.js
@@ -2,6 +2,8 @@ const session = require('express-session');
 const connectSessionKnex = require('connect-session-knex');
 const {knex} = require('db/');
 
+const environment = process.env.NODE_ENV || 'development';
+
 const sessions = () => {
     const KnexSessionStore = connectSessionKnex(session);
     const store = new KnexSessionStore({knex, tablename: 'sessions', sidfieldname: 'session_id'});
@@ -9,16 +11,13 @@ const sessions = () => {
     return session({
         name: 'session_id',
         secret: process.env.TOKEN_SECRET,
-        resave: false,
-        saveUninitialized: true,
+        resave: false, // Don't resave the session if it was not changed
+        saveUninitialized: false,
         rolling: true, // Automatically extends the session age on each request.
         cookie: {
             httpOnly: true,
             maxAge: 20 * 60 * 1000, // 20 minutes
-            // recommended you use this setting in production if you have a well-known domain you want to restrict the cookies to.
-            // domain: 'your.domain.com',
-            // recommended you use this setting in production if your site is published using HTTPS
-            // secure: true,
+            secure: environment === 'production', // Only allow https connections in production
         },
         store,
     });
