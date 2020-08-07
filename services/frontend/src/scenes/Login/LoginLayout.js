cbaacea07b90f1872267106831875d017de90cd0
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-08-06 22:30:15 -0400

    Refactor 'Separator' and 'Card' into their own components.

--- services/frontend/src/scenes/Login/LoginLayout.js
+++ services/frontend/src/scenes/Login/LoginLayout.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Logo, LoadingButton, GithubLoginButton, TextField, FormError} from 'components/';
+import {Logo, LoadingButton, GithubLoginButton, TextField, Separator, Card} from 'components/';
+import {IS_PRODUCTION} from 'utils/environment/';
 import './Login.scss';
 
 const LoginLayout = (props) => (
@@ -10,41 +11,29 @@ const LoginLayout = (props) => (
 );
 
 const LoginCard = ({emailInput, passwordInput, onSubmit, isLoading, errorMessage}) => (
-    <div className='login-card'>
-        <div className='card-title'>
-            <h1 className='card-section'>Login</h1>
+    <Card className='login-card' title='Login' errorMessage={errorMessage}>
+        <div className='social-buttons-container'>
+            <GithubLoginButton className='github-button' />
         </div>
 
-        <Separator />
+        {!IS_PRODUCTION && (
+            <React.Fragment>
+                <div className='or-separator'>
+                    <Separator />
+                    <span className='or'>OR</span>
+                    <Separator />
+                </div>
 
-        <FormError message={errorMessage} />
-
-        <div className='form-container card-section'>
-            <div className='social-buttons-container'>
-                <GithubLoginButton className='github-button' />
-            </div>
-
-            <div className='or-separator'>
-                <Separator />
-                <span className='or'>OR</span>
-                <Separator />
-            </div>
-
-            <div className='email-password-container'>
                 <TextField className='form-input' label='Email' type='email' {...emailInput} />
                 <TextField className='form-input' label='Password' type='password' {...passwordInput} />
-            </div>
 
-            <div className='buttons-container'>
-                <LoadingButton className='login-button' label='login' onClick={onSubmit} isLoading={isLoading} />
-                <button className='signup-button'>create account</button>
-            </div>
-        </div>
-    </div>
-);
-
-const Separator = () => (
-    <div className='separator' />
+                <div className='buttons-container'>
+                    <LoadingButton className='login-button' label='login' onClick={onSubmit} isLoading={isLoading} />
+                    <button className='signup-button'>create account</button>
+                </div>
+            </React.Fragment>
+        )}
+    </Card>
 );
 
 export default LoginLayout;
