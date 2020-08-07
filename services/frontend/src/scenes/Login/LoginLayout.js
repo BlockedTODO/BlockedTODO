64bef4c6037620099564e57e37ac56816830e966
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-08-06 20:40:36 -0400

    Style the "log in with github" button.

--- services/frontend/src/scenes/Login/LoginLayout.js
+++ services/frontend/src/scenes/Login/LoginLayout.js
@@ -11,28 +11,35 @@ const LoginLayout = (props) => (
 
 const LoginCard = ({emailInput, passwordInput, onSubmit, isLoading, errorMessage}) => (
     <div className='login-card'>
-        <CardTitle />
+        <div className='card-title'>
+            <h1 className='card-section'>Login</h1>
+        </div>
+
+        <Separator />
+
         <FormError message={errorMessage} />
 
         <div className='form-container card-section'>
-            <GithubLoginButton />
+            <div className='social-buttons-container'>
+                <GithubLoginButton className='github-button' />
+            </div>
 
-            <div>---- OR ----</div>
+            <div className='or-separator'>
+                <Separator />
+                <span className='or'>OR</span>
+                <Separator />
+            </div>
 
-            <TextField className='form-input' label='Email' type='email' {...emailInput} />
-            <TextField className='form-input' label='Password' type='password' {...passwordInput} />
+            <div className='email-password-container'>
+                <TextField className='form-input' label='Email' type='email' {...emailInput} />
+                <TextField className='form-input' label='Password' type='password' {...passwordInput} />
+            </div>
 
             <div className='buttons-container'>
                 <LoadingButton className='login-button' label='login' onClick={onSubmit} isLoading={isLoading} />
                 <button className='signup-button'>create account</button>
             </div>
         </div>
-    </div>
-);
-
-const CardTitle = () => (
-    <div className='card-title'>
-        <h1 className='card-section'>Login</h1>
     </div>
 );
 
