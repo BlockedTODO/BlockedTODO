41930a37feacfd86d3e4c70edd8807fd0b3a4270
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-08-30 00:17:14 -0400

    Add useOutsideCloseable hook.

--- services/frontend/src/hooks/index.js
+++ services/frontend/src/hooks/index.js
@@ -2,3 +2,4 @@ export {default as useInput} from './useInput';
 export {default as useRestClient} from './useRestClient';
 export {default as useGraphqlClient} from './useGraphqlClient';
 export {default as useIsLoggedIn} from './useIsLoggedIn';
+export {default as useOutsideCloseable} from './useOutsideCloseable';
--- services/frontend/src/hooks/useIsLoggedIn.js
+++ services/frontend/src/hooks/useIsLoggedIn.js
@@ -5,7 +5,7 @@ import restClient from 'utils/restClient';
  * Since we use http-only cookies for sessions, we can't directly
  * check if a session is active without making a request to the backend.
  * So we have to keep track of whether we were previously logged in to populate the default state.
- * We then call the backend to verify authentication on every re-render (TODO: move that part here)*/
+ * We then call the backend to verify authentication on every re-render */
 const useIsLoggedIn = () => {
     // Get the initial "logged in" state from localStorage
     const initialState = localStorage.getItem('isLoggedIn') === 'true';
