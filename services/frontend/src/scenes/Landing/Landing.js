5085f09054220a6c61ca2d2a4a4fc877d0c15521
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-08-30 01:21:44 -0400

    Fix issue with calling a hook from inside a render function.
    Error message: Cannot update during an existing state transition (such as within `render`). Render methods should be a pure function of props and state.

--- services/frontend/src/scenes/Landing/Landing.js
+++ services/frontend/src/scenes/Landing/Landing.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react';
-import {Link, useHistory} from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
 import {Logo, Card, Separator} from 'components/';
 import {loggedInContext} from 'Store';
 import './Landing.scss';
@@ -7,36 +7,35 @@ import './Landing.scss';
 /* To avoid issues with readinessProbes in GKE, the root path shouldn't redirect on regular (unauthenticated) requests
  * so we just show this landing page on requests to '/' and redirect to /repositories if the user is authenticated.
  * Details: https://cloud.google.com/kubernetes-engine/docs/concepts/ingress#limitations */
-const Landing = () => {
+const Landing = ({location}) => {
     const [isLoggedIn] = useContext(loggedInContext);
-    const history = useHistory();
-
-    if (isLoggedIn) {
-        history.push('/repositories');
-    }
 
     return (
-        <div className='landing'>
-            <Logo className='landing-logo' />
+        isLoggedIn ? (
+            <Redirect to={{pathname: '/repositories', state: {from: location}}} />
+        ) : (
+            <div className='landing'>
+                <Logo className='landing-logo' />
 
-            <Card className='landing-card' title='Frontend App'>
-                <div className='buttons-container'>
-                    <Link className='link-button' to={{pathname: 'https://blockedtodo.com'}} target='_blank'>
-                        visit the official website
-                    </Link>
+                <Card className='landing-card' title='Frontend App'>
+                    <div className='buttons-container'>
+                        <Link className='link-button' to={{pathname: 'https://blockedtodo.com'}} target='_blank'>
+                            visit the official website
+                        </Link>
 
-                    <div className='or-separator'>
-                        <Separator />
-                        <span className='or'>OR</span>
-                        <Separator />
+                        <div className='or-separator'>
+                            <Separator />
+                            <span className='or'>OR</span>
+                            <Separator />
+                        </div>
+
+                        <Link className='link-button' to='/login'>
+                            log in
+                        </Link>
                     </div>
-
-                    <Link className='link-button' to='/login'>
-                        log in
-                    </Link>
-                </div>
-            </Card>
-        </div>
+                </Card>
+            </div>
+        )
     );
 };
 
