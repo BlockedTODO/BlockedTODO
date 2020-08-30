92e38c6f5b423fe0f94d6af09b99171d7ecc237b
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-08-29 23:25:05 -0400

    Add account circle, logout, and settings icons.

--- services/frontend/src/assets/icons/index.js
+++ services/frontend/src/assets/icons/index.js
@@ -1,3 +1,6 @@
 export {ReactComponent as ToDoIcon} from './ToDoIcon.svg';
 export {ReactComponent as AccountBoxIcon} from './AccountBoxIcon.svg';
+export {ReactComponent as AccountCircleIcon} from './AccountCircleIcon.svg';
 export {ReactComponent as GithubIcon} from './GithubIcon.svg';
+export {ReactComponent as LogoutIcon} from './LogoutIcon.svg';
+export {ReactComponent as SettingsIcon} from './SettingsIcon.svg';
