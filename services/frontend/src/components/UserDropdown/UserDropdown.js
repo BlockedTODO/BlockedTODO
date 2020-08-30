3a61a8815556384231054476daac4afb41b1174d
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-08-30 01:23:03 -0400

    Implement logout button functionality.

--- services/frontend/src/components/UserDropdown/UserDropdown.js
+++ services/frontend/src/components/UserDropdown/UserDropdown.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {useHistory} from 'react-router-dom';
+import {loggedInContext} from 'Store';
+import restClient from 'utils/restClient';
+import {useRestClient} from 'hooks';
 import UserDropdownLayout from './UserDropdownLayout';
 
 const UserDropdown = ({isOpen}) => {
     const history = useHistory();
+    const setIsLoggedIn = useContext(loggedInContext)[1];
 
     const onSettingsClick = () => history.push('/404');
-    const onLogoutClick = () => history.push('/404');
+    const {run: onLogoutClick} = useRestClient({
+        deferFn: () => restClient.post('/auth/logout'),
+        onResolve: () => setIsLoggedIn(false)
+    });
 
     return (
         <UserDropdownLayout
