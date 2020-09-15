80e30d6dc07b3f23fc71ecf1df4f39ce5d1b545b
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-09-15 15:43:12 -0400

    Replace withTempDir util with tempy.directory.task (new in tempy 0.7.0).

--- services/backend/src/parser/getConfig.test.js
+++ services/backend/src/parser/getConfig.test.js
@@ -1,5 +1,6 @@
 const getConfig = require('./getConfig');
-const {withTempDirectory, DEFAULT_CONFIG} = require('utils/');
+const tempy = require('tempy');
+const {DEFAULT_CONFIG} = require('utils/');
 const {promises: fsPromises} = require('fs');
 
 const configText = `
@@ -11,7 +12,7 @@ const validatedConfig = {comment_prefixes: ['prefix1', 'Second prefix']};
 
 describe('getConfig', () => {
     it('reads a valid config file', async () => {
-        await withTempDirectory(async (codeFolder) => {
+        await tempy.directory.task(async (codeFolder) => {
             await fsPromises.writeFile(`${codeFolder}/.blockedtodo.yaml`, configText);
             const config = await getConfig(codeFolder);
 
@@ -20,7 +21,7 @@ describe('getConfig', () => {
     });
 
     it('returns the default config when the provided config file has invalid syntax', async () => {
-        await withTempDirectory(async (codeFolder) => {
+        await tempy.directory.task(async (codeFolder) => {
             const text = 'invalid yaml syntax';
             await fsPromises.writeFile(`${codeFolder}/.blockedtodo.yaml`, text);
             const config = await getConfig(codeFolder);
@@ -30,7 +31,7 @@ describe('getConfig', () => {
     });
 
     it('returns the default config when no config file is present', async () => {
-        await withTempDirectory(async (codeFolder) => {
+        await tempy.directory.task(async (codeFolder) => {
             const config = await getConfig(codeFolder);
             expect(config).toMatchObject(DEFAULT_CONFIG);
         });
