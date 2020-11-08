6f0d56294839aca3b29067804615efb7cf72ed1a
Dominic Roy-Stang <dominicroystang@gmail.com>
2020-11-08 01:48:00 -0500

    Remove react imports since they are no longer needed with the new JSX transform.

--- services/website/src/App.js
+++ services/website/src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import AppRouter from './AppRouter';
 import './App.scss';
 
--- services/website/src/scenes/Policy/Policy.js
+++ services/website/src/scenes/Policy/Policy.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import './Policy.scss';
 
 const Policy = () => (
