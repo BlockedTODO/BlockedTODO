ba27eef4b23f056f3f3bc7116302ffa9b8338474
Dominic Roy-Stang <dominicroystang@gmail.com>
2021-06-06 13:30:37 -0400

    Remove comment that is no longer relevant since the upgrade to ECMAScript Module

--- services/backend/src/db/index.js
+++ services/backend/src/db/index.js
@@ -1,5 +1,3 @@
-/* BlockedTODO: https://github.com/knex/knex/issues/3866
- * Upgrade knex and use absolute imports all over the db/ folder once it's closed */
 import {Model} from 'objection';
 import Knex from 'knex';
 import knexConfig from './config/knexfile.js';
