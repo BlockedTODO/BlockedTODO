37970f76c444df5055085e056255f42d998857a7
Dominic Roy-Stang <dominicroystang@gmail.com>
2022-07-02 01:08:50 -0700

    Remove dockerize, replace it with wait-for.sh
    Source of wait-for.sh script: https://github.com/eficode/wait-for

--- services/backend/scripts/docker-start.sh
+++ services/backend/scripts/docker-start.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+PARENT_FOLDER=$(dirname "$0")
+
 # Wait for services
-dockerize -wait tcp://$DATABASE_HOST:$DATABASE_PORT -timeout 1m
+$PARENT_FOLDER/wait-for.sh $DATABASE_HOST:$DATABASE_PORT --timeout 60 \
+  -- echo "Database is running." || echo "Timed out waiting for the database."
 
 # Run migrations
 npm run db:migrate
