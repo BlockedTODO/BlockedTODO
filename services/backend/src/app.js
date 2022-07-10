d9b45a2140deb3c57d4f8ef2628d94663b8f3418
Dominic Roy-Stang <dominicroystang@gmail.com>
2022-07-09 18:32:44 -0700

    Revert "Remove /health backend endpoint."
    This reverts commit b44b35aadf36735a83d592e99a7f07ca58d36001.

--- services/backend/src/app.js
+++ services/backend/src/app.js
@@ -21,6 +21,7 @@ app.use(githubWebhooks);
 
 app.get('/', (req, res, next) => res.send('BlockedTODO Backend Server'));
 app.get('/ping', (req, res, next) => res.send('Pong!'));
+app.get('/health', (req, res, next) => res.send('OK'));
 
 app.use('/auth', authRouter);
 app.use('/github', githubRouter);
