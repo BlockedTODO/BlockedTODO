6f7867460d501da51cb799ef8b77b505d61f32d0
Dominic Roy-Stang <dominicroystang@gmail.com>
2022-10-29 20:25:33 -0700

    Update scanCodebase file to use metadata in logs.

--- services/backend/src/parser/scanCodebase.js
+++ services/backend/src/parser/scanCodebase.js
@@ -1,4 +1,3 @@
-import stringifyObject from 'stringify-object';
 import {logger} from '../utils/index.js';
 import parseCodebase from './parseCodebase.js';
 import {deleteUnreferencedIssues, createMissingIssues} from './issueHandler.js';
@@ -6,32 +5,32 @@ import {createMissingTasks} from './taskHandler.js';
 
 /* Perform a complete scan of a codebase. */
 const scanCodebase = async (codeFolder, repository, githubClient) => {
-    logger.info(`Gathering list of referenced issues for ${repository.id}`);
+    logger.info('Gathering list of referenced issues for repo', {repository});
     const referencedIssues = await parseCodebase(codeFolder);
-    logger.info(`Referenced issues in repo ${repository.id}: ${stringifyObject(referencedIssues)}`);
+    logger.info('Referenced issues in repo', {referencedIssues});
 
     // Get repository and log its related issues
     repository = await repository.$query().withGraphFetched('issues');
 
-    logger.info(`Deleting unreferenced issues in ${stringifyObject(repository)}`);
+    logger.info('Deleting unreferenced issues in repo', {repository});
     await deleteUnreferencedIssues(repository, referencedIssues);
 
     // Refresh repository so that deleted issues are removed
     repository = await repository.$query().withGraphFetched('issues');
-    logger.info(repository);
+    logger.info({repository});
 
-    logger.info(`Creating missing issues in repo ${repository.id}`);
-    await createMissingIssues(repository, Object.keys(referencedIssues));
-    logger.info(`Missing issues created for repo ${repository.id}`);
+    logger.info('Creating missing issues', {repository, referencedIssues});
+    const issues = await createMissingIssues(repository, Object.keys(referencedIssues));
+    logger.info('Missing issues created', {repository, issues});
 
     // Refresh repository so that created issues are included
     repository = await repository.$query().withGraphFetched('issues');
-    logger.info(repository);
+    logger.info({repository});
 
     // Create tasks
-    logger.info(`Creating missing tasks in repo ${repository.id}`);
-    await createMissingTasks(repository, githubClient, referencedIssues);
-    logger.info(`Missing tasks created for repo ${repository.id}`);
+    logger.info('Creating missing tasks', {repository});
+    const tasks = await createMissingTasks(repository, githubClient, referencedIssues);
+    logger.info('Missing tasks created', {repository, tasks});
 };
 
 export default scanCodebase;
