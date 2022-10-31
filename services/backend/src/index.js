c7afa7ba2c37c4b41e6c327e771d51332b6c059a
Dominic Roy-Stang <dominicroystang@gmail.com>
2022-10-30 18:24:42 -0700

    Fix circular references in logs and display non-enumerable properties of errors in logs.

--- services/backend/src/index.js
+++ services/backend/src/index.js
@@ -4,7 +4,7 @@ import app from './app.js';
 const server = app.listen(config.backend.internalPort);
 
 process.on('unhandledRejection', (error) => {
-    logger.error(`Unhandled Rejection at Promise: ${error.message}.`, {error});
+    logger.error(`Unhandled rejection at Promise: ${error.message}.`, {error});
 });
 
 server.on('listening', () => {
